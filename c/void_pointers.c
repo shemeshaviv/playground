b0a56ad00d5d7848bd1269d39cb0f10312112ca4
Aviv Shemesh <shemeshaviv@gmail.com>
2023-05-08 09:08:32 +0300

    Print addresses as well

--- c/void_pointers.c
+++ c/void_pointers.c
@@ -8,10 +8,10 @@ int main(int argc, char* argv[])
     void* vptr;
 
     vptr = &i;
-    printf("vptr points to an integer with value: %d\n", *(int *)vptr);
+    printf("vptr points to an int  (address: %p) with value: %d\n",(int*) vptr, *(int *)vptr);
     vptr = &f;
-    printf("vptr points to a float with value: %.2f\n", *(float *)vptr);
+    printf("vptr points to a float (address: %p) with value: %.2f\n",(float*) vptr, *(float *)vptr);
     vptr = &c;
-    printf("vptr points to a char with value: %c\n", *(char *)vptr);
+    printf("vptr points to a char  (address: %p) with value: %c\n",(char*) vptr, *(char *)vptr);
     return 0;
 }
